119bc8373d00d29f585284e95e65145ecc062f1d
Maciej Szajna <maciej.szajna@polecat.com>
2022-06-14 11:15:36 +0000

    Dump 1

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Datomic dbfn
 
-Facilities for development of Datomic database functions.
+Facilities for development of Datomic transaction functions.
+
+Datomic ships with the following transaction functions:
+
+:db/add - idempotently record a datom enforcing schema, uniqueness and referential integrity,
+:db/retract - idempotently retract a datom,
+:db/retractEntity - :db/retract all attributes of and references to an entity if it exists,
+:db/cas - asserts the current value of an entity's attribute of :db.cardinality/one,
+          then :db/add or abort transaction.
+
+The set of transaction functions available shapes the way you model your data.
+
+
+===
+
+
 
 ## License
 
--- src/datomic/dbfn.clj
+++ src/datomic/dbfn.clj
@@ -1,79 +1,120 @@
 (ns datomic.dbfn
-  (:refer-clojure :exclude [fn defn])
-  (:require [datomic.api :as d]
-            [datomic.function :refer [compile-clojure]]))
+  (:require [clojure.walk :as w]
+            [backtick :as b]
+            [datomic.api :as d]))
 
-(clojure.core/defn fn-spec
-  "Given quoted: map m of requires and imports, params and Clojure code,
-   attempts to compile the code to catch any problems and returns a map
-   suitable for datomic.api/function."
-  [m params code]
-  (let [{:keys [imports requires]} m]
-    (when (empty? params)
-      (throw (new IllegalArgumentException
-                  "#db/fn needs at least one argument for db value.")))
-    (compile-clojure imports requires params (str code))
-    (assoc m :lang "clojure" :params params :code code)))
+(def mfunction
+  "Memoized d/function, installed on transactor automatically by call.
+   Use invalidate-fn-cache to invalidate."
+  (memoize d/function))
 
-(defmacro fn
+(def ^:private mf `mfunction)
+
+(defn force-fn
+  "Force db-fn created with datomic.api/function to compile immediately."
+  [db-fn]
+  (force (:fnref db-fn))
+  db-fn)
+
+(defn quote-unquotes [form]
+  (let [quot (fn [x] `'~x)]
+    (w/postwalk
+    #(cond
+        (b/unquote? %) (quot %)
+        (b/unquote-splicing? %) (list* (first %) (map quot (rest %)))
+        :else %)
+    form)))
+
+(defn ser
+  "Serializes the db-fn for use in another txn."
+  [db-fn]
+  (cond-> (dissoc db-fn :fnref)
+    (empty? (:requires db-fn)) (dissoc :requires)
+    (empty? (:imports db-fn)) (dissoc :imports)))
+
+(defn quoted-dbfn? [form]
+  (and (seq? form)
+       (= (first form) 'quote)
+       (instance? datomic.function.Function (second form))))
+
+(defn ser-dbfns [form]
+  (let [c (gensym "c")
+        ser-dbfn-form #(if (quoted-dbfn? %) `(~c (quote ~(ser (second %)))) %)
+        updated (w/postwalk ser-dbfn-form form)]
+    (if (not= updated form)
+     `(let* [~c (resolve '~mf) ~c (if ~c @~c ~`d/function)] ~updated)
+      form)))
+
+(defmacro txn
   "Returns a Datomic #db/fn of params that evaluates to body. Note that
    body is not a closure and has to require/import any vars outside
    [clojure.core :refer :all], [datomic.api :as d] and java.lang.* before use
    with the optional requires+imports? map under :requires and :imports.
+   Local context can be accessed in compile time using unquote.
 
    Usage:
    Run #db/fn locally
-   => (let [f (dbfn/fn [db arg] ...)] (f (d/db conn) arg-val))
+   => (let [f (txn [db arg] ..)] (f (d/db conn) arg-val))
 
    Use in transactions once installed
-   => (d/transact conn [{:db/ident :my-fn :db/fn (dbfn/fn [db arg] ...)}])
-   => (d/transact conn [[:myfn arg-val]]) ; db argument is applied implicitly,
+   => (d/transact conn [{:db/ident :my-fn :db/fn (txn [db arg] ..)}])
+   => (d/transact conn [[:my-fn arg-val]]) ; db argument is applied implicitly,
 
    Use in transactions directly with :datomic.dbfn/call (see call in this ns)
-   => (dbfn/def myfn [db arg] ...)
-   => (d/transact conn [[::dbfn/call myfn arg-val]])
+   => (d/transact conn [[:datomic.dbfn/call (txn [db arg] ..) arg-val]])
 
    See also https://docs.datomic.com/on-prem/database-functions.html"
   {:arglists '([requires+imports? [params*] body])}
   [& args]
-  (let [[m params & body] (if (map? (first args)) args `(nil ~@args))
+  (let [[{:keys [requires imports]} params & body]
+          (if (map? (first args)) args `(nil ~@args))
         code (if (= 1 (count body)) (first body) `(do ~@body))]
-   `(d/function (fn-spec '~m '~params '~code))))
+   `(-> {:lang :clojure
+         :requires '~requires
+         :imports '~imports
+         :params '~params
+         :code (pr-str (ser-dbfns (b/template ~(quote-unquotes code))))}
+        d/function
+        force-fn)))
 
-(defmacro defn
-  "Same as (def name doc-string? (datomic.dbfn/fn [params*] body))."
+(defmacro deftxn
+  "Same as (def name doc-string? (txn [params*] body))."
   {:arglists '([name doc-string? requires+imports? [params*] body])}
   [name & args]
   (let [[doc & args] (if (string? (first args)) args `(nil ~@args))]
-   `(def ~name ~@(when doc [doc]) (datomic.dbfn/fn ~@args))))
+   `(def ~name ~@(when doc [doc]) (txn ~@args))))
 
-(datomic.dbfn/defn call
+(defn- ns-sym [s] (symbol (namespace s)))
+(defn- name-sym [s] (symbol (name s)))
+
+(deftxn call
   "Compiles the db-fn using datomic.api/function and applies it db and args.
-   Caches compilation on the transactor indefinitely, unless invalidated
-   with invalidate-dbfn-cache.
+  Caches compilation on the transactor indefinitely, unless invalidated
+  with invalidate-fn-cache.
 
-   Usage:
-   => (d/transact conn [dbfn/call-install-tx]) ; run once
-   => (d/transact conn [[::dbfn/call (dbfn [db arg] ...) arg-val]])"
-  [db db-fn & args]
-  (let [compile
-        @(or (resolve 'datomic.dbfn/dbfn-cache)
-             (intern (create-ns 'datomic.dbfn)
-                     'dbfn-cache
-                     (memoize d/function)))]
-    (apply (compile db-fn) db args)))
+  Usage:
+  => (require '[datomic.dbfn :as t])
+  => (d/transact conn [t/call-install-tx]) ; run once
+  => (t/deftxn my-tx [db arg] ..)
+  => (d/transact conn [[::t/call my-tx arg-val]])"
+  [db dbfn & args]
+  (let [compile-dbfn
+        @(or (resolve ~mf)
+              (intern (create-ns ~(ns-sym mf)) ~(ns-sym mf)
+                      (memoize d/function)))]
+    (apply (compile-dbfn dbfn) db args)))
 
-(datomic.dbfn/defn invalidate-dbfn-cache
-  "Invalidate the compilation cache of :datomic.dbfn/call. Datomic serializes
-   #db/fn to a map which is then used as the cache key guaranteeing that the
-   cache never goes stale. #db/fns don't close over the lexical scope and so
-   tend not to occupy much memory either. You should rarely need this function.
-   
-   Usage:
-   => (d/transact conn [[::dbfn/call dbfn/invalidate-dbfn-cache]])"
+(deftxn invalidate-fn-cache
+  "Invalidate the compilation cache of call. Datomic serializes #db/fn to a map
+  which is then used as the cache key guaranteeing that the cache never goes
+  stale. #db/fns don't close over the lexical scope and so tend not to occupy
+  much memory either. You should rarely need this function.
+
+  Usage:
+  => (d/transact conn [[:datomic.dbfn/call invalidate-dbfn-cache]])"
   [_]
-  (ns-unmap 'datomic.dbfn 'dbfn-cache))
+  (ns-unmap (create-ns ~(ns-sym mf)) ~(name-sym mf)))
 
 (def call-install-tx
   "Transaction statement to install :datomic.dbfn/call, see call in this namespace."
-  {:db/ident :datomic.dbfn/call :db/fn call})
+  {:db/ident ::call :db/fn call})
