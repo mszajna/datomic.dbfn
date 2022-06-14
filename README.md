3700d29390672d44a98581066b0b6d9e7726334d
Maciej Szajna <maciej.szajna@polecat.com>
2022-06-14 11:15:50 +0000

    Dump 2

--- README.md
+++ README.md
@@ -2,20 +2,34 @@
 
 Facilities for development of Datomic transaction functions.
 
-Datomic ships with the following transaction functions:
+Datomic ships with the following primitives:
 
 :db/add - idempotently record a datom enforcing schema, uniqueness and referential integrity,
 :db/retract - idempotently retract a datom,
+
+and transaction functions:
 :db/retractEntity - :db/retract all attributes of and references to an entity if it exists,
 :db/cas - asserts the current value of an entity's attribute of :db.cardinality/one,
           then :db/add or abort transaction.
 
 The set of transaction functions available shapes the way you model your data.
 
-
 ===
 
+## Motivation for custom txn macro
 
+It is frustrating how each transaction function definition is an island, with its own
+objects, abstractions and dependencies. You can obviously still call previously installed
+fns generating adequate tx statement.
+
+If you wanted to reuse db/fn code using d/function you'd build quoted snippets
+of code and embed them using syntax unquote. You could also unquote function calls
+generating snippets, which is exactly what macros do in regular code.
+
+Resolving macros on the client is viable and could allow some code reuse but is
+quite limiting. Most third-party macros resolve to some function call you can't
+expect to be available on the transactor. There is pure macros like -> but they're
+first party anyway (available on transactor too).
 
 ## License
 
--- src/datomic/dbfn.clj
+++ src/datomic/dbfn.clj
@@ -1,6 +1,8 @@
 (ns datomic.dbfn
   (:require [clojure.walk :as w]
             [backtick :as b]
+            [riddley.walk :as r]
+            [riddley.compiler :as rc]
             [datomic.api :as d]))
 
 (def mfunction
@@ -35,15 +37,105 @@
 (defn quoted-dbfn? [form]
   (and (seq? form)
        (= (first form) 'quote)
-       (instance? datomic.function.Function (second form))))
+       (instance? datomic.function.Function (second form))
+       (= "clojure" (name (:lang (second form))))))
 
-(defn ser-dbfns [form]
+; TODO: try serializing dbfns as inline functions instead of d/functions.
+;       - need to unify requires. Perhaps could only support :as requires?
+;       - at which point perhaps could infer reqs, but what about imports?
+(defn ser-dbfns' [form]
   (let [c (gensym "c")
         ser-dbfn-form #(if (quoted-dbfn? %) `(~c (quote ~(ser (second %)))) %)
         updated (w/postwalk ser-dbfn-form form)]
     (if (not= updated form)
      `(let* [~c (resolve '~mf) ~c (if ~c @~c ~`d/function)] ~updated)
       form)))
+
+; This does the trick, bar handling requires/imports.
+(defn ser-dbfns [form]
+  (let [inline (fn [f] `(~'fn ~(:params f) ~(read-string (:code f))))]
+    (w/postwalk
+      #(if (quoted-dbfn? %) (inline (second %)) %)
+      form)))
+
+; Requires can be unified assuming they're aliased.
+; Conflicting imports can't be resolved without qualifying
+; all symbols, which requires macroexpanding-all.
+; An option is to allow inlining only when imports and requires don't conflict.
+(defn unify-reqs [reqs])
+
+; Alternatively I could macroexpand and provide a d/transaction middleware to
+; attempt not to send the entire function, just its hash or smth.
+; Would have to use a strong hashing function
+(defn with-cache [tr]
+  (fn [conn tx]
+    (letfn [use-fn-hash-if-exists
+            ([st]
+              (if (and (= (first st) ::invoke) (:hash (second st)))
+                (dissoc (update st 1 :hash) :fnref)
+                st))]
+      (try (tr conn (map use-fn-hash-if-exists tx))
+        (catch Throwable t ; TODO: narrow down the catch
+          (tr conn tx))))))
+
+; walk the form and return all global vars & classes referenced
+(defn walk [form env]
+  (cond
+    (symbol? form) (when-let [r (resolve env form)] [r])
+    (seq? form)
+    (let [head (first form)
+          resolved (resolve env head)]
+      (cond
+        (and resolved (.isMacro resolved))
+        (concat [resolved] (walk (apply resolved form env (rest form)) env))
+        (= 'fn* head)
+        (for [[binds & body] (drop-while #(not (seq? %)) form)
+              :let [env (merge env (zipmap binds (repeat true)))]]
+              res (walk body env)]
+          res)
+        (= 'let* head)
+        (let [[[bind exp & binds] & body] (rest form)]
+          (if bind
+            (concat (walk exp env)
+                    (walk `(let* [~binds] ~@body) (assoc env bind true)))
+            (mapcat #(walk % env) body)))
+        (= 'loop* head) (walk `(let* ~@(rest form)) env)
+        (= 'letfn* head)
+        (let [binds (partition 2 (second form))
+              env (merge env (zipmap (map first binds) (repeat true)))]
+          (concat (mapcat #(walk (second %) env) binds)
+                  (mapcat #(walk % env) (drop 2 form))))
+        ; TODO: try/catch/finally
+        :else (mapcat #(walk % env) form)))
+    (or (vector? form) (map? form) (set? form)) (mapcat #(walk % env) form)))
+
+
+(defmacro txn*
+  "A variant of txn that blurs the line of local/transactor divide even further
+   with body macro-expanded on client, requires and imports inferred from
+   *ns* and symbols from namespaces unavailable on transactor automatically (?)
+   inlined, including inlining of db-fns. txn*s cannot refer regular non-db fns."
+  [params & body]
+  ; TODO: use riddley to macroexpand, resolve symbols, inline db-fns as regular
+  ; functions unifying requires and imports, inline locals and vars for ns that
+  ; are not whitelisted.
+  ; Consideration: many macros expand to code referencing non-core symbols.
+  ; Auto-inline doesn't help much as these are mostly fn-calls, resulting
+  ; in confusing errors about not being able to embed objects in code. I could
+  ; detect these problems early for warnings to name the offending symbol but
+  ; tracing it back to source code is still tricky as the symbol comes from
+  ; macro-expansion.
+
+  ; Reg. riddley - it seems riddley eagerly macroexpands - there's no way to capture
+  ; macro symbols for 'requires' with it
+  nil)
+
+; IDEA: install db functions under their hash with a middleware?
+; Embedding functions is technically possible, if inconvenient. It feels like
+; a lot of work and trade-offs for a solution that is a leaky abstraction
+; (vars resolved to values eagerly - can't redef; function bodies grow
+; uncontrollably). I could register all defined dbfns at compilation time,
+; and install them on startup or smth perhaps?
 
 (defmacro txn
   "Returns a Datomic #db/fn of params that evaluates to body. Note that
